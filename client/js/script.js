c588c60949786d85786b8551dfeb6cc6ba207cec
Antônio Pedro <antoniopedro.rs9@gmail.com>
2021-03-16 23:10:31 -0300

    Refatoração do código JS e simplificação do CSS

--- client/js/script.js
+++ client/js/script.js
@@ -1,48 +1,55 @@
+window.onload = () => {
+	api.get("unos").then((response) => {
+		renderUnos(response);
+		renderChart(response);
+	});
+}
+
+
+
+function renderUnos(response) {
+	let unos = response.data;
+
+	for (i in unos) {
+		let div = document.createElement("div");
+
+		div.className = "card theme-dark";
+
+		div.innerHTML = `
+			<img src="./img/uno_${unos[i].name.toLowerCase()}.png">
+			<h6>Uno ${unos[i].name}</h6>
+			<p>Bla bla bla bla</p>
+			<button class="theme-pink" onclick="handleVotes(${unos[i].id})">Votar</button>
+		`;
+		document.getElementById("container").appendChild(div);
+	}
+}
+
+
+
 let chart;
 
-window.onload = function renderUnos() {
-	api.get("unos").then((response) => {
-		let unos = response.data;
+function renderChart(response) {
+	let votes = response.data.map((uno) => uno.votes);
+	let ctx = document.getElementById("myChart").getContext("2d");
 
-		for (i in unos) {
-			let div = document.createElement("div");
-
-			div.className = "card theme-dark";
-
-			div.innerHTML = `
-				<img src="./img/uno_${unos[i].name.toLowerCase()}.png">
-				<h6>Uno ${unos[i].name}</h6>
-				<p>Bla bla bla bla</p>
-				<button class="theme-deep-purple" onclick="handleVotes(${unos[i].id})">Votar</button>
-			`;
-			
-			document.getElementById("container").appendChild(div);
+	chart = new Chart(ctx, {
+		type: "bar",
+		data: {
+			labels: ["Antigo", "Clássico", "Turbo", "Novo"],
+			datasets: [{ data: votes, backgroundColor: "#e91e63" }],
+		},
+		options: {
+			legend: { display: false },
+			scales: {
+				yAxes: [{ ticks: { beginAtZero: true } }]
+			}
 		}
 	});
+	Chart.defaults.global.defaultFontColor = "#fff";
 }
 
-function renderChart() {
-	api.get("unos").then((response) => {
-		let votes = response.data.map((uno) => uno.votes);
-		let ctx = document.getElementById("myChart").getContext("2d");
 
-		chart = new Chart(ctx, {
-			type: "bar",
-			data: {
-				labels: ["Antigo", "Clássico", "Turbo", "Novo"],
-				datasets: [{ data: votes, backgroundColor: "#673ab7" }],
-			},
-			options: {
-				legend: { display: false },
-				scales: {
-					yAxes: [{ ticks: { beginAtZero: true } }]
-				}
-			}
-		});
-		Chart.defaults.global.defaultFontColor = "#fff";
-	});
-}
-renderChart();
 
 function handleVotes(id) {
 	api.put(`unos/vote/${id}`).then(() => {
@@ -50,6 +57,7 @@ function handleVotes(id) {
 
 		api.get("unos").then((response) => {
 			let votes = response.data.map((uno) => uno.votes);
+			
 			chart.data.datasets[0].data = votes;
 			chart.update();
 		});
